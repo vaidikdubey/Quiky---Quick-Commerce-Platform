cae63856d7ed0dcf56f8774a371e513f8be43ff3
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-03-12 17:27:23 +0530

    Added basic express initialization with middlewares

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL ?? process.env.BASE_URL,
+        credentials: true,
+        methods: ["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Content-Disposition",
+        ],
+    }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+export default app;
---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env",
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, console.log(`Server is running on PORT: ${PORT}🌐`));
