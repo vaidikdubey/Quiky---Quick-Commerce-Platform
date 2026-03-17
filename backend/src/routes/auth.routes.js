93355fd2df80981b98fc237501afe78e271fd38d
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-03-17 19:30:22 +0530

    Added auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,5 +2,34 @@ import { Router } from "express";
 
 const router = Router();
 
+router
+  .route("/register")
+  .post(userRegistrationValidator(), validate, registerUser);
+
+router.route("/login").post(userLoginValidator(), validate, loginUser);
+
+router.route("/verify/:token").get(verifyUser);
+
+router.route("/me").get(isLoggedIn, getProfile);
+
+router.route("/logout").get(isLoggedIn, logoutUser);
+
+router
+  .route("/forgot-password")
+  .post(forgotPasswordValidator(), validate, forgotPassword);
+
+router
+  .route("/reset-password/:token")
+  .post(resetPasswordValidate(), validate, resetPassword);
+
+router.route("/resend-verification").get(isLoggedIn, resendVerificationEmail);
+
+router
+  .route("/change-password")
+  .post(isLoggedIn, changePasswordValidate(), validate, changePassword);
+
+router.route("/update-profile").patch(isLoggedIn, updateProfile);
+
+router.route("/delete-user").delete(isLoggedIn, deleteProfile);
 
 export default router;
