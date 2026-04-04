9418966c3b95d064d6bb488f296993b2753e5c1d
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-04 17:12:27 +0530

    Added all product routes

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,6 +1,34 @@
 import { Router } from "express";
+import { isLoggedIn, checkPermission } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// CLIENT;
+// RIDER;
+// STORE_MANAGER;
+// ADMIN;
+
+const permissions = ["STORE_MANAGER", "ADMIN"];
+
+router.route("/getAll/:storeId").get(isLoggedIn, getAllProducts);
+
+router.route("/get/:id").get(isLoggedIn, getProductById);
+
+//Public route for anyone to search any product by it's name
+router.route("/search").get(getProductByName);
+
+router.route("/nearby").get(isLoggedIn, getProductsInNearbyStores);
+
+router
+  .route("/create")
+  .post(isLoggedIn, checkPermission(permissions), createProduct);
+
+router
+  .route("/update/:id")
+  .patch(isLoggedIn, checkPermission(permissions), updateProduct);
+
+router
+  .route("/delete/:id")
+  .delete(isLoggedIn, checkPermission(permissions), deleteProduct);
 
 export default router;
