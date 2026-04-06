9350fb06e48281c4af457ba89446ec7bb082b15a
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-06 12:30:14 +0530

    Added get all products controller

--- backend/src/controllers/product.controllers.js
+++ backend/src/controllers/product.controllers.js
@@ -3,7 +3,89 @@ import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler";
 
-const getAllProducts = asyncHandler(async (req, res) => {});
+const getAllProducts = asyncHandler(async (req, res) => {
+  const { storeId } = req.params;
+
+  if (!storeId) throw new ApiError(404, "Store id not found");
+
+  const existingStore = await db.store.findUnique({
+    where: {
+      id: storeId,
+    },
+    select: {
+      name: true,
+      address: true,
+      latitude: true,
+      longitude: true,
+      pincode: true,
+      isActive: true,
+      manager: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+        },
+      },
+      _count: {
+        select: {
+          products,
+          orders,
+        },
+      },
+    },
+  });
+
+  if (!existingStore || !existingStore.isActive)
+    throw new ApiError(404, "Store not found");
+
+  const allProducts = await db.product.findMany({
+    where: {
+      storeId,
+    },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      price: true,
+      imageUrl: true,
+      stock: true,
+      isAvailable: true,
+      category: true,
+      _count: {
+        select: {
+          orderItems: true,
+        },
+      },
+    },
+  });
+
+  if (!allProducts) throw new ApiError(404, "No products found");
+
+  const data = allProducts.map((product) => {
+    return {
+      ...product,
+      store: {
+        name: existingStore.name,
+        address: existingStore.address,
+        latitude: existingStore.latitude ? existingStore.latitude : null,
+        longitude: existingStore.longitude ? existingStore.longitude : null,
+        pincode: existingStore.pincode,
+        isActive: existingStore.isActive,
+        manager: {
+          id: existingStore.manager.id,
+          name: existingStore.manager.name,
+          email: existingStore.manager.email,
+          phone: existingStore.manager.phone,
+        },
+        productCount: existingStore._count.products,
+        orderCount: existingStore._count.orders,
+      },
+    };
+  });
+
+  res.status(200).json(new ApiResponse(200, data, "Products found"));
+});
 
 const getProductById = asyncHandler(async (req, res) => {});
 
