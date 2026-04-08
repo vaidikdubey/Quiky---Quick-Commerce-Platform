5a28ec06817c537e52d2ec901071eb54996d9dc9
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-08 12:16:15 +0530

    Added all order routes

--- backend/src/controllers/order.controllers.js
+++ backend/src/controllers/order.controllers.js
@@ -2,3 +2,24 @@ import { db } from "../db/db.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
+
+const createOrder = asyncHandler(async (req, res) => {});
+
+const getAllOrders = asyncHandler(async (req, res) => {});
+
+const getOrderById = asyncHandler(async (req, res) => {});
+
+const updateOrderStatus = asyncHandler(async (req, res) => {});
+
+const cancelOrder = asyncHandler(async (req, res) => {});
+
+const getAllOrdersForStore = asyncHandler(async (req, res) => {});
+
+export {
+  createOrder,
+  getAllOrders,
+  getOrderById,
+  updateOrderStatus,
+  cancelOrder,
+  getAllOrdersForStore,
+};
--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,5 +1,27 @@
 import { Router } from "express";
+import { isLoggedIn, checkPermission } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
+const clientPermission = ["CLIENT", "ADMIN"];
+const managerPermission = ["STORE_MANAGER", "ADMIN"];
+
+router
+  .route("/create")
+  .post(isLoggedIn, checkPermission(clientPermission), createOrder);
+
+router.route("/myOrders").get(isLoggedIn, getAllOrders);
+
+router.route("/getOrder/:id").get(isLoggedIn, getOrderById);
+
+router
+  .route("/updateStatus/:id")
+  .patch(isLoggedIn, checkPermission(managerPermission), updateOrderStatus);
+
+router.route("/cancel/:id").post(isLoggedIn, cancelOrder);
+
+router
+  .route("/storeOrders/:storeId")
+  .get(isLoggedIn, checkPermission(managerPermission), getAllOrdersForStore);
 
 export default router;
