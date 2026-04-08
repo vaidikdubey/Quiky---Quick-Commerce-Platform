f88f66fa4a1ce3d3c186ad0b3c98c9ee92281932
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-08 13:02:45 +0530

    Added get all orders controller

--- backend/src/controllers/order.controllers.js
+++ backend/src/controllers/order.controllers.js
@@ -69,7 +69,82 @@ const createOrder = asyncHandler(async (req, res) => {
   res.status(201).json(new ApiResponse(201, newOrder, "New order created"));
 });
 
-const getAllOrders = asyncHandler(async (req, res) => {});
+const getAllOrders = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+
+  const allOrders = await db.order.findMany({
+    where: {
+      clientId: id,
+    },
+    select: {
+      id: true,
+      clientId: true,
+      storeId: true,
+      riderId: true,
+      totalAmount: true,
+      status: true,
+      paymentMethod: true,
+      paymentStatus: true,
+      deliveryAddress: true,
+      createdAt: true,
+      client: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+        },
+      },
+      store: {
+        select: {
+          id: true,
+          name: true,
+          address: true,
+          pincode: true,
+          manager: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      },
+      rider: {
+        select: {
+          id: true,
+          totalDeliveries: true,
+          rating: true,
+          createdAt: true,
+          user: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+      delivery: {
+        select: {
+          status: true,
+          pickupTime: true,
+          deliveryTime: true,
+          estimatedTime: true,
+        },
+      },
+      _count: {
+        select: {
+          items: true,
+        },
+      },
+    },
+  });
+
+  if (!allOrders || allOrders.length < 0)
+    throw new ApiError(400, "Error fetching all orders");
+
+  if (allOrders.length === 0)
+    return res.status(200).json(new ApiResponse(200, [], "No orders found"));
+
+  res.status(200).json(new ApiResponse(200, allOrders, "All orders fetched"));
+});
 
 const getOrderById = asyncHandler(async (req, res) => {});
 
