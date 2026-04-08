abc425604bd8d6cdbc31efedf4a77b32412e0080
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-08 15:27:12 +0530

    Added set default address controller

--- backend/src/controllers/address.controllers.js
+++ backend/src/controllers/address.controllers.js
@@ -1,4 +1,5 @@
 import { db } from "../db/db.js";
+import { isDbNull } from "../generated/prisma/runtime/client.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
@@ -15,11 +16,30 @@ const addNewAddress = asyncHandler(async (req, res) => {
     city,
     state,
     landmark,
-    isDefault,
+    isDefault = false,
   } = req.body;
 
   if (!fullAddress || !pincode)
     throw new ApiError(400, "Please provide full address and pincode");
+
+  if ((latitude && !longitude) || (!latitude && longitude))
+    throw new ApiError(
+      400,
+      "Both latitude and longitude must be provided together",
+    );
+
+  //If current address is marked default, unmark all other addresses
+  if (isDefault) {
+    await db.address.updateMany({
+      where: {
+        id,
+        isDefault: true,
+      },
+      data: {
+        isDefault: false,
+      },
+    });
+  }
 
   const data = {};
 
@@ -32,9 +52,9 @@ const addNewAddress = asyncHandler(async (req, res) => {
   if (city) data.city = city;
   if (state) data.state = state;
   if (landmark) data.landmark = landmark;
-  if (isDefault) data.isDefault = isDefault;
+  if (isDefault) data.isDefault = Boolean(isDefault);
 
-  const newAddress = await db.address.create({
+  let newAddress = await db.address.create({
     data,
     select: {
       id: true,
@@ -51,6 +71,36 @@ const addNewAddress = asyncHandler(async (req, res) => {
   });
 
   if (!newAddress) throw new ApiError(500, "Error adding new address");
+
+  //If this is the first address mark it as default
+  const addressCount = await db.address.count({
+    where: {
+      id,
+    },
+  });
+
+  if (addressCount === 1) {
+    newAddress = await db.address.update({
+      where: {
+        id: newAddress.id,
+      },
+      data: {
+        isDefault: true,
+      },
+      select: {
+        id: true,
+        label: true,
+        fullAddress: true,
+        pincode: true,
+        latitude: true,
+        longitude: true,
+        city: true,
+        state: true,
+        landmark: true,
+        isDefault: true,
+      },
+    });
+  }
 
   res
     .status(201)
@@ -72,17 +122,36 @@ const updateAddress = asyncHandler(async (req, res) => {
     isDefault,
   } = req.body;
 
+  if ((latitude && !longitude) || (!latitude && longitude))
+    throw new ApiError(
+      400,
+      "Both latitude and longitude must be provided together",
+    );
+
+  //If current address is marked default, unmark all other addresses
+  if (isDefault) {
+    await db.address.updateMany({
+      where: {
+        id,
+        isDefault: true,
+      },
+      data: {
+        isDefault: false,
+      },
+    });
+  }
+
   const data = {};
 
   if (label) data.label = label;
   if (fullAddress) data.fullAddress = fullAddress;
-  if (latitude) data.latitude = latitude;
-  if (longitude) data.longitude = longitude;
+  if (latitude) data.latitude = parseFloat(latitude);
+  if (longitude) data.longitude = parseFloat(longitude);
   if (pincode) data.pincode = pincode;
   if (city) data.city = city;
   if (state) data.state = state;
   if (landmark) data.landmark = landmark;
-  if (isDefault) data.isDefault = isDefault;
+  if (isDefault) data.isDefault = Boolean(isDefault);
 
   const updatedAddresss = await db.address.update({
     where: {
@@ -180,7 +249,60 @@ const getAddressById = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse(200, address, "Address found"));
 });
 
-const setDefaultAddress = asyncHandler(async (req, res) => {});
+const setDefaultAddress = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+
+  //Mark all addresses as non-default
+  await db.address.updateMany({
+    where: {
+      userId,
+      isDefault: true,
+    },
+    data: {
+      isDefault: false,
+    },
+  });
+
+  const defaultAddress = await db.address.update({
+    where: {
+      id,
+    },
+    data: {
+      isDefault: true,
+    },
+    select: {
+      id: true,
+      label: true,
+      fullAddress: true,
+      latitude: true,
+      longitude: true,
+      pincode: true,
+      city: true,
+      state: true,
+      landmark: true,
+      isDefault: true,
+      createdAt: true,
+      updatedAt: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+        },
+      },
+    },
+  });
+
+  if (!defaultAddress) throw new ApiError(500, "Error setting default address");
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, defaultAddress, "Default address set successfully"),
+    );
+});
 
 const deleteAddress = asyncHandler(async (req, res) => {});
 
