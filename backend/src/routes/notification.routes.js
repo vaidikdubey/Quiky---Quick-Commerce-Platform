02a56b8effb964507ea328015ea95b7dbbbfc513
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-11 13:34:24 +0530

    Added notification routes

--- backend/src/routes/notification.routes.js
+++ backend/src/routes/notification.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
+router.route("/getAll").get(isLoggedIn, getAllNotifications);
+
+router.route("/unread").get(isLoggedIn, getAllUnreadNotifications);
+
+router.route("/read/:id").patch(isLoggedIn, markNotificationRead);
+
+router.route("/delete/:id").delete(isLoggedIn, deleteNotification);
 
 export default router;
