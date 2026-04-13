639fd7024be1e0d0fa1bea93299899dd59e7acbd
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-13 21:46:51 +0530

    Added rider routes and controllers

--- backend/src/controllers/rider.controllers.js
+++ backend/src/controllers/rider.controllers.js
@@ -1,4 +1,25 @@
-import { db } from "../db/db.js"
+import { db } from "../db/db.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
+
+const getProfile = asyncHandler(async (req, res) => {});
+
+const updateProfile = asyncHandler(async (req, res) => {});
+
+const getRiderRating = asyncHandler(async (req, res) => {});
+
+const getAllDeliveries = asyncHandler(async (req, res) => {});
+
+const getDeliveryById = asyncHandler(async (req, res) => {});
+
+const getRiderEarnings = asyncHandler(async (req, res) => {});
+
+export {
+  getProfile,
+  updateProfile,
+  getRiderRating,
+  getAllDeliveries,
+  getDeliveryById,
+  getRiderEarnings,
+};
--- backend/src/routes/rider.routes.js
+++ backend/src/routes/rider.routes.js
@@ -1,5 +1,37 @@
 import { Router } from "express";
+import { isLoggedIn, checkPermission } from "../middlewares/auth.middleware.js";
+import {
+  getAllDeliveries,
+  getDeliveryById,
+  getProfile,
+  getRiderEarnings,
+  getRiderRating,
+  updateProfile,
+} from "../controllers/rider.controllers.js";
 
 const router = Router();
+
+const riderPermission = ["RIDER", "ADMIN"];
+
+router
+  .route("/profile")
+  .get(isLoggedIn, checkPermission(riderPermission), getProfile)
+  .patch(isLoggedIn, checkPermission(riderPermission), updateProfile);
+
+router
+  .route("/rating")
+  .get(isLoggedIn, checkPermission(riderPermission), getRiderRating);
+
+router
+  .route("/allDeliveries")
+  .get(isLoggedIn, checkPermission(riderPermission), getAllDeliveries);
+
+router
+  .route("/getDelivery/:id")
+  .get(isLoggedIn, checkPermission(riderPermission), getDeliveryById);
+
+router
+  .route("/earnings")
+  .get(isLoggedIn, checkPermission(riderPermission), getRiderEarnings);
 
 export default router;
