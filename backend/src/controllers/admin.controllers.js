017a0ae13796476f809df7ba9bb5215532ad00a4
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-13 22:45:55 +0530

    Added all admin routes and controllers

--- backend/src/controllers/admin.controllers.js
+++ backend/src/controllers/admin.controllers.js
@@ -2,3 +2,21 @@ import { db } from "../db/db.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
+
+const getAllUsers = asyncHandler(async (req, res) => {});
+
+const getAllStores = asyncHandler(async (req, res) => {});
+
+const getAllRiders = asyncHandler(async (req, res) => {});
+
+const getAllOrders = asyncHandler(async (req, res) => {});
+
+const toggleUserAccount = asyncHandler(async (req, res) => {});
+
+export {
+  getAllUsers,
+  getAllStores,
+  getAllRiders,
+  getAllOrders,
+  toggleUserAccount,
+};
--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,5 +1,25 @@
 import { Router } from "express";
+import { checkAdmin, isLoggedIn } from "../middlewares/auth.middleware.js";
+import {
+  getAllOrders,
+  getAllRiders,
+  getAllStores,
+  getAllUsers,
+  toggleUserAccount,
+} from "../controllers/admin.controllers.js";
 
 const router = Router();
+
+router.route("/users").get(isLoggedIn, checkAdmin, getAllUsers);
+
+router.route("/stores").get(isLoggedIn, checkAdmin, getAllStores);
+
+router.route("/riders").get(isLoggedIn, checkAdmin, getAllRiders);
+
+router.route("/orders").get(isLoggedIn, checkAdmin, getAllOrders);
+
+router
+  .route("/users/:id/status")
+  .patch(isLoggedIn, checkAdmin, toggleUserAccount);
 
 export default router;
