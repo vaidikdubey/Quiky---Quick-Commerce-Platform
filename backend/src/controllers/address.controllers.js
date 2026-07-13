78f668bf2d2d382688097657797c8e1ce5c45f88
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-13 20:08:22 +0530

    Removed unused import

--- backend/src/controllers/address.controllers.js
+++ backend/src/controllers/address.controllers.js
@@ -1,5 +1,4 @@
 import { db } from "../db/db.js";
-import { isDbNull } from "../generated/prisma/runtime/client.js";
 import { ApiError } from "../utils/api-error.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
